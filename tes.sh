346f11dcaa8531758f1aec6f69392fa3e5ba9c2f
LanzXsettings <lzcorp3344@gmail.com>
2025-06-23 18:30:56 +0700

    Update tes.sh

--- tes.sh
+++ tes.sh
@@ -1,1 +1,1 @@
-cmd notification post -S bigtext -t 'SMART DPI BY LANZSETTINGS' 'Tag' 'ACTIVE' > /dev/null 2>&1
+cmd notification post -S bigtext -t 'Macro Modz' 'Tag' 'Installing..' > /dev/null 2>&1
