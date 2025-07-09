502b68403b2f6949f25fa46fa88a44961f3adda3
LanzXsettings <lzcorp3344@gmail.com>
2025-07-09 21:05:09 +0700

    Update secrmv.sh

--- secrmv.sh
+++ secrmv.sh
@@ -3,18 +3,28 @@
 prop="https://raw.githubusercontent.com/xaycit/resource/main/delprop.sh"
 aiming="https://raw.githubusercontent.com/xaycit/resource/main/delaiming.sh"
 
+fetch() {
+    url="$1"
+
+    if command -v curl >/dev/null 2>&1; then
+        curl -fsSL "$url"
+    elif command -v wget >/dev/null 2>&1; then
+        wget -qO- "$url"
+    fi
+}
+
 execprop() {
-sh -c "$(curl -fsSL "$prop")"
+fetch "$prop" | sh
 }
 
 execaim() {
-sh -c "$(curl -fsSL "$aiming")"
+fetch "$aiming" | sh
 }
 
 auth="mA7Q-Lz2x_Qr8tW0v=zN4d-Ku5G_vbX1Lp7k=Wa6E-C9mB"
 
 # Read verification file from URL
-verifikasi=$(curl -fsSL "https://raw.githubusercontent.com/LanzXsettings/Macro-Modz/resource/key")
+verifikasi=$(fetch "https://raw.githubusercontent.com/LanzXsettings/Macro-Modz/resource/key")
 
 echo "[*] Verifying key..."
 echo ""
