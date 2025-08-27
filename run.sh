447b2cfb27d9a823e8872f8fe2f863bd4bc876e8
LanzXsettings <lzcorp3344@gmail.com>
2025-08-27 13:04:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -107,7 +107,7 @@ get_device_info() {
     echo "Android Version : $ANDROID_VERSION"
     echo "Developer       : LanzSettings"
     echo "Credit          : Xay Citter"
-    echo "File Version    : V2"
+    echo "File Version    : V3"
     echo "File Type       : Ultimate"
     echo ""
 }
